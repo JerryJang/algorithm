00d6b2f6127c18bcc8c7c542cce42424f46448d8
Jang Si Young <sysmile77@gmail.com>
2024-10-15 22:44:46 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 209ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241015/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void recursive(int n) {
+
+        if(n ==0) return;
+
+        for(int i=0; i<n; i++){
+
+            System.out.print("* ");
+
+        }
+        System.out.println();
+
+        recursive(n-1);
+
+        for(int j=0; j<n; j++){
+
+            System.out.print("* ");
+        }
+        System.out.println();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        recursive(n);
+
+    }
+}
