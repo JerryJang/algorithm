1d5517963c4b3bbbad67dc6fc0b32f265b6aacac
Jang Si Young <sysmile77@gmail.com>
2024-10-16 20:52:59 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241016/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static int solution(int n) {
+
+        if(n == 1 ) return 2;
+        if(n == 2 ) return 4;
+
+
+        return (solution(n-1) * solution(n-2))%100;
+        
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.print(solution(n));
+    
+    }
+
+
+
+}
