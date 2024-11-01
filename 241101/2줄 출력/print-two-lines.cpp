002f5d102c00f76daeaf852c0353dbb28d35078f
Jang Si Young <sysmile77@gmail.com>
2024-11-01 21:22:51 +0900

    Title: 2줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241101/2줄 출력/print-two-lines.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+
+    cout<<"Hello";
+    cout<< endl;
+    cout << "World";
+
+    // cout<<"Hello"<<endl<<"World";
+
+    // cout<<"Hello";
+    // cout<<"\n";
+    // cout<<"World";
+
+    return 0;
+}
