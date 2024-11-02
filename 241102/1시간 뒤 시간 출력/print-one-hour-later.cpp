de84095209c65f0b24dc0638390a3b92f88883d2
Jang Si Young <sysmile77@gmail.com>
2024-11-02 18:55:48 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a,b;
+    char c;
+
+    cin>>a;
+    a = a+1;
+    cin.get();
+    cin>>b;
+
+    cout<<a<<":"<<b;
+    return 0;
+}
