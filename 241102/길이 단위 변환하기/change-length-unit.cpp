af421e4a6b0b2a4b00e77b5edf378326e7354bda
Jang Si Young <sysmile77@gmail.com>
2024-11-02 16:55:07 +0900

    Title: 길이 단위 변환하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+
+    double fi = 30.48;
+    double mile = 160934;
+
+    cout<<"9.2ft = ";
+    cout<<fixed;
+    cout.precision(1);
+
+    cout<<fi*9.2<<"cm"<<endl;
+
+
+    cout<<"1.3mi = "<<mile*1.3<<"cm";
+
+
+    return 0;
+}
